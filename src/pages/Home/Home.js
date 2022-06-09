d70915e8558166d05a8f011dfdcd0c14100c16ee
mert <mertkiymazaslan2001@gmail.com>
2022-06-09 22:51:04 +0200

    commit

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,29 @@
-import { Container, TextField, Typography } from "@material-ui/core";
-import React from "react";
+import { Button, Container, MenuItem, TextField, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { makeStyles } from "@material-ui/core";
 import { findByLabelText } from "@testing-library/react";
 import "./Home.css";
+import axios from "axios";
 
 const useStyles = makeStyles({
-  nameInput: {
+  textField: {
     marginBottom: "25px",
   },
 });
 
 const Home = () => {
   const classes = useStyles();
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+
+  useEffect(() => {
+    axios.get("https://opentdb.com/api_category.php").then((response) => {
+      setCategories(response.data.trivia_categories);
+    });
+  }, []);
+
 
   return (
     <div className="content">
@@ -22,10 +33,48 @@ const Home = () => {
 
         <div className="settings_select">
           <TextField
-            className={classes.nameInput}
+            className={classes.textField}
             label="Enter Your Name"
             variant="outlined"
           />
+
+          <TextField
+            className={classes.textField}
+            select
+            label="Select Category"
+            variant="outlined"
+            value={selectedCategory}
+            onChange={(e) => setSelectedCategory(e.target.value)}
+          >
+            {categories.map((category) => (
+              <MenuItem key={category.id} value={category.id}>
+                {category.name}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            className={classes.textField}
+            select
+            label="Select Difficulty"
+            variant="outlined"
+            value={difficulty}
+            onChange={(e) => setDifficulty(e.target.value)}
+          >
+            <MenuItem key={"Easy"} value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key={"Medium"} value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key={"Hard"} value="hard">
+              Hard
+            </MenuItem>
+          </TextField>
+
+          <Button variant="contained" color="primary" size="large">
+            Start Quiz
+          </Button>
         </div>
       </div>
     </div>
